73400343f1fbf14b64c485eb3d822c6884c947f1
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2025-01-31 20:55:05 -0500

    Started documentation structure
    No content yet.

--- 1.21.1/verdant/neoforge/src/main/java/com/startraveler/verdant/Verdant.java
+++ 1.21.1/verdant/neoforge/src/main/java/com/startraveler/verdant/Verdant.java
@@ -70,7 +70,6 @@ public class Verdant {
 
     public Verdant(final IEventBus eventBus) {
 
-
         // This method is invoked by the NeoForge mod loader when it is ready
         // to load your mod. You can access NeoForge and Common code in this
         // project.
